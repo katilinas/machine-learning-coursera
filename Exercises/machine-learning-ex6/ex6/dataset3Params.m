de5d2e0bd5d439293234ad9f765ac6ed43526c31
Siarhei Antaniuk <katilinas@gmail.com>
2020-10-15 19:39:02 -0700

    ex6 50% done
    Signed-off-by: Siarhei Antaniuk <katilinas@gmail.com>

--- Exercises/machine-learning-ex6/ex6/dataset3Params.m
+++ Exercises/machine-learning-ex6/ex6/dataset3Params.m
@@ -22,12 +22,33 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+x1 = [1 2 1]; x2 = [0 4 -1];
+C_range = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+sigma_range = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 
+size_C = size(C_range,2);
+size_sigma = size(sigma_range,2);
 
+output = [1000 1000 1000];
 
+for i = 1:size_C
+  C = C_range(i);
+  
+  for j = 1:size_sigma
+    sigma = sigma_range(j);
+    
+    model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+    predictions = svmPredict(model, Xval);
+    error = mean(double(predictions ~= yval));
+    
+    output = [output; C sigma error];
+  endfor
+endfor
 
-
-
+[min_val, min_index] = min(output(:,3));
+C = output(min_index,1);
+sigma = output(min_index,2);
+error = output(min_index,3);
 
 % =========================================================================
 
