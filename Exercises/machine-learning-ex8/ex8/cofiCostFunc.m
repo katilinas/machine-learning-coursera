fb4407e58a9a185cba3db0f12811e8faa6710445
Siarhei Antaniuk <katilinas@gmail.com>
2020-10-25 18:57:36 -0700

    ex8 80% done
    Signed-off-by: Siarhei Antaniuk <katilinas@gmail.com>

--- Exercises/machine-learning-ex8/ex8/cofiCostFunc.m
+++ Exercises/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -45,14 +45,20 @@ J = sum(sum(((X*Theta' - Y).^2).*R))/2;
 
 
 
+for i=1:num_movies
+    idx = find(R(i, :)==1);
+    Theta_temp = Theta(idx, :);
+    Y_temp = Y(i, idx);
+    X_grad(i, :) = (X(i, :)* Theta_temp' - Y_temp)* Theta_temp;
+endfor
 
 
-
-
-
-
-
-
+for j=1:num_users
+    idx = find(R(:, j)==1);
+    X_temp = X(idx, :);
+    Y_temp = Y(idx, j);
+    Theta_grad(j, :) = ((X_temp* Theta(j, :)' - Y_temp))'* X_temp;
+endfor
 
 
 % =============================================================
